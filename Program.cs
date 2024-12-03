8208236dc573a63be14a883e2cf3749966477654
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-03 10:10:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment_5._2._1
+{
+    internal class Program
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            int length = 0;
+            bool isCounting = false;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    length++;
+                    isCounting = true;
+                }
+                else if (isCounting)
+                {
+                    break;
+                }
+            }
+
+            return length;
+        }
+        static void Main(string[] args)
+        {
+            string s1 = "Hello World";
+            Console.WriteLine(LengthOfLastWord(s1));
+
+            // Example 2
+            string s2 = "   fly me   to   the moon  ";
+            Console.WriteLine(LengthOfLastWord(s2));
+
+            // Example 3
+            string s3 = "a";
+            Console.WriteLine(LengthOfLastWord(s3));
+        }
+    }
+}
+
